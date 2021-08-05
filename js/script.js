b03f4ed93ef9548ea04936c07fe3e8e323386b15
Belay Adamu <belayadamu11@gmail.com>
2021-08-05 10:07:29 +0300

    almost ready for submission

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@ const humburgers = document.querySelectorAll('.humburgers');
 let clicked = true;
 
 navButton.addEventListener('click', () => {
+    
     navButton.classList.toggle('changeBg');
     navBar.classList.toggle('disp_none');
-    for (i=0; i < humburgers.length; i++ ){
+    
+    for (i = 0; i < humburgers.length; i++ ){
        humburgers[i].classList.toggle('clicked');
     } 
     body.classList.toggle('disable_scroll');
+    
     setTimeout(()=>{
         navLinks.classList.toggle('isOpen');
     }, 0.01);
