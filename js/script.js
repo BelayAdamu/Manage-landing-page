b29b22eed9a7ab391b6e68a95f5807bed1951e05
Belay Adamu <belayadamu11@gmail.com>
2021-08-06 12:58:52 +0300

    Script complete

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const navBar = document.querySelector('.navbar');
 const navLinks = document.querySelector('.nav-links');
 const body = document.querySelector('.body');
 const humburgers = document.querySelectorAll('.humburgers');
+const emailInput = document.querySelector('.inbox_input');
+const goButton = document.querySelector('.submit');
+const errMsg = document.querySelector('.err_p');
 
 let clicked = true;
 
@@ -19,4 +22,14 @@ navButton.addEventListener('click', () => {
     setTimeout(()=>{
         navLinks.classList.toggle('isOpen');
     }, 0.01);
-})
+});
+
+goButton.addEventListener('click', ()=>{
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)){
+        if(errMsg.classList.contains('noErr_msg')){
+            errMsg.classList.remove('noErr_msg');
+        }
+    } else{
+        errMsg.classList.add('noErr_msg');
+    }
+});
